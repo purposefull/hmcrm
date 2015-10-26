a15f90126feb009307e3270a26c2a5cfcfbf8657
andreybolonin <andreybolonin1989@gmail.com>
2015-10-26 13:04:52 +0300

    added sonata adminuserbundle

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Sonata\UserBundle\Entity\BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+}
