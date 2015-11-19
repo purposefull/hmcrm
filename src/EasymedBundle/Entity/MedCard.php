95cc6c13c1bbe041ba7e0d3e49716d69a68421c5
andreybolonin <andreybolonin1989@gmail.com>
2015-11-19 10:34:13 +0300

    update

---
+++ src/EasymedBundle/Entity/MedCard.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace EasymedBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * MedCard
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class MedCard
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="recommendation", type="string", length=255)
+     */
+    private $recommendation;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="intake", type="string", length=255)
+     */
+    private $intake;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set recommendation
+     *
+     * @param string $recommendation
+     *
+     * @return MedCard
+     */
+    public function setRecommendation($recommendation)
+    {
+        $this->recommendation = $recommendation;
+
+        return $this;
+    }
+
+    /**
+     * Get recommendation
+     *
+     * @return string
+     */
+    public function getRecommendation()
+    {
+        return $this->recommendation;
+    }
+
+    /**
+     * Set intake
+     *
+     * @param string $intake
+     *
+     * @return MedCard
+     */
+    public function setIntake($intake)
+    {
+        $this->intake = $intake;
+
+        return $this;
+    }
+
+    /**
+     * Get intake
+     *
+     * @return string
+     */
+    public function getIntake()
+    {
+        return $this->intake;
+    }
+}
+
